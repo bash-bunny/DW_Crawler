20d4f75217d27e10d33328e17bc9ec9f24fe826e
bash-bunny <bashbunny@paranoidsec.com>
2025-11-18 16:50:11 +0100

    Updated crawler

--- dw_crawl.sh
+++ dw_crawl.sh
@@ -1,25 +1,22 @@
-#!/bin/sh
-
-# Anew without golang
-xanew() { awk 'hit[$0]==0 {hit[$0]=1; print $0}'; }
-which anew &>/dev/null || alias anew=xanew
+#!/usr/bin/env sh
 
 # Create the files
 URLLIST=$(mktemp urllist.XXXXXX)
 URLLIST2=$(mktemp urllist.XXXXXX)
 SEEN="seen"
+touch ${SEEN}
 
 # Feed the URLLIST with the seeds
 cat seed > "$URLLIST"
-cat "$URLLIST" | anew "$SEEN"
+cat "$URLLIST" > "$SEEN"
 
 while [ -s "$URLLIST" ] ; do
 	cat "$URLLIST" |
-		parallel proxychains4 -q lynx -listonly -image_links -dump {} \; \
+		parallel -j100 proxychains4 -q lynx -listonly -image_links -dump {} \; \
 			echo Spidered: {} \>\&2 |
 			perl -ne 's/#.*//; s/\s+\d+.\s(\S+)$/$1/ and do { $seen{$1}++ or print }' |
 			perl -ne 'print if m|^(?:https?://)?(?:[a-zA-Z0-9-]+\.)?[a-zA-Z0-9-]+\.onion(?:/.*)?$|' |
-		awk 'NR==FNR{seen[$0]; next} !($0 in seen)' "$SEEN" - | anew "$SEEN" > "$URLLIST2"
+		awk 'NR==FNR{seen[$0]; next} !($0 in seen)' "$SEEN" - | tee -a "$SEEN" > "$URLLIST2"
 	mv "$URLLIST2" "$URLLIST"
 done
 
